7cc1151d44c6ae44f14a3439d2f096461ef98211
mrseramadhan <mrseramadhan@gmail.com>
2020-10-06 11:32:30 +0700

    upload

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -533,6 +533,8 @@ class ComposerStaticInit4cd45e732f01ef9c9bd431633c25a361
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Library\\Tunnel' => __DIR__ . '/../..' . '/app/Library/Token.php',
+        'App\\Models\\Model_Movie' => __DIR__ . '/../..' . '/app/Models/Model_Movie.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
